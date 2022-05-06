936c92f10d057a9b4499309ada741c5959a34ca5
OLAMIDE OLAGUNJU <olamxii@gmail.com>
2022-05-06 08:16:53 +0100

    change image path

--- app.py
+++ app.py
@@ -142,7 +142,7 @@ def main():
     #     # emrlogo = Image.open('cpu.png')
     #     st.image('cpu.png',width = 200, caption = '')
     with st.sidebar.container():
-        image = Image.open('cpu.png')
+        image = Image.open(f'C:/Users/user/Desktop/streamlit/cpu.png')
         st.image(image,width = 200)
 
     selected = option_menu(
